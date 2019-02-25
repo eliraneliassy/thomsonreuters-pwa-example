e3eb8b64b65cd67ea7d7712839441881852bb294
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-02-25 14:17:42 +0200

    add pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private sw: SwUpdate) { }
   title = 'tr-pwa-project';
+
+  ngOnInit(): void {
+    if (this.sw.isEnabled) {
+      this.sw.available.subscribe((event) => {
+        console.log(event);
+        if (confirm('Press a button!')) {
+          window.location.reload();
+        } else {
+          console.log('cancel');
+        }
+      });
+    }
+  }
 }
